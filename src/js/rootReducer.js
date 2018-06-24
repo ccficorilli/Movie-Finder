6dcef35ee42b936a0d520f577e128b5589d761e1
ccficorilli <ccficorilli@gmail.com>
2018-06-24 07:41:31 -0700

    rearranging the files back to folders

--- src/js/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import search from './reducers/searchReducer';
-
-const rootReducer = combineReducers({
-// add reducers
-  search
-});
-
-export default rootReducer;
